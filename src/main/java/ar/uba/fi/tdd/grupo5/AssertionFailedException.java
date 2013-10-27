d7cc51b76192690accd46f07a6cf767c7648f6f0
Ezequiel PD <ezeperez26@gmail.com>
2013-10-27 12:25:54 -0300

    Elimino la advertencia de AssertionFailedException y corrijo el codigo.

--- src/main/java/ar/uba/fi/tdd/grupo5/AssertionFailedException.java
+++ src/main/java/ar/uba/fi/tdd/grupo5/AssertionFailedException.java
@@ -1,21 +1,14 @@
 package ar.uba.fi.tdd.grupo5;
 
-
 public class AssertionFailedException extends Exception {
-  
-	public AssertionFailedException(String msg)
-    {
-        super(msg);
-    }
-	
-	public AssertionFailedException()
-    {
-        super("Message was not provided");
-    }
 
-	public String getMessage()
-    {
-        return super.getMessage();
-    }
-	
+	private static final long serialVersionUID = -847889422986201396L;
+
+	public AssertionFailedException(String msg) {
+		super(msg);
+	}
+
+	public AssertionFailedException() {
+		super("Message was not provided");
+	}
 }
