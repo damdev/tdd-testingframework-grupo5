026dddc2570cfc9d03d6ceedeef9d104d83a78bb
Diego R. Montoya <diegormontoya@hotmail.com>
2013-10-28 07:46:54 +0000

    Agrego tests de AssertionFailedException. Pequeños cambios en la clase pertinente

--- src/main/java/ar/uba/fi/tdd/grupo5/framework/AssertionFailedException.java
+++ src/main/java/ar/uba/fi/tdd/grupo5/framework/AssertionFailedException.java
@@ -3,12 +3,13 @@ package ar.uba.fi.tdd.grupo5.framework;
 public class AssertionFailedException extends Exception {
 
 	private static final long serialVersionUID = -847889422986201396L;
+	private static final String defaultMessage = "Message was not provided";
 
 	public AssertionFailedException(String msg) {
 		super(msg);
 	}
 
 	public AssertionFailedException() {
-		super("Message was not provided");
+		super(defaultMessage);
 	}
 }
--- src/test/java/ar/uba/fi/tdd/grupo5/TestAssertionFailedException.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/TestAssertionFailedException.java
@@ -4,16 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestAssertionFailedException {
+import ar.uba.fi.tdd.grupo5.framework.AssertionFailedException;
 
+public class TestAssertionFailedException {
+	
+	private final String message = "Exception message";
+	private final String defaultMessage = "Message was not provided";
+	
 	@Test
 	public void testAssertionFailedExceptionString() {
-		fail("Not yet implemented");
+		AssertionFailedException tester = new AssertionFailedException(message);
+		assertEquals("AssertionFailedException.getMessage() returns a different message than the constructor parameter", tester.getMessage(), message);
 	}
 
 	@Test
 	public void testAssertionFailedException() {
-		fail("Not yet implemented");
+		AssertionFailedException tester = new AssertionFailedException();
+		assertEquals("AssertionFailedException.getMessage() returns a different default message than the constructor defaults", tester.getMessage(), defaultMessage);
 	}
-
 }
