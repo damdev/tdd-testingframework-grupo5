c4d2d47be73032e7027d2a893619c318ad020739
Ezequiel PD <ezeperez26@gmail.com>
2013-10-29 01:27:31 -0300

    Corrigo los nombres de los métodos de AssertionFailedExceptionTest para que tengan nombres más descriptivos.

--- src/test/java/ar/uba/fi/tdd/grupo5/AssertionFailedExceptionTest.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/AssertionFailedExceptionTest.java
@@ -1,25 +1,27 @@
 package ar.uba.fi.tdd.grupo5;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import ar.uba.fi.tdd.grupo5.framework.AssertionFailedException;
 
 public class AssertionFailedExceptionTest {
-	
+
 	private final String message = "Exception message";
 	private final String defaultMessage = "Message was not provided";
-	
+
 	@Test
-	public void testAssertionFailedExceptionString() {
+	public void personalizedExceptionMessage() {
 		AssertionFailedException tester = new AssertionFailedException(message);
-		assertEquals("AssertionFailedException.getMessage() returns a different message than the constructor parameter", tester.getMessage(), message);
+		assertEquals("AssertionFailedException.getMessage() returns a "
+				+ "different message than the constructor parameter", message,
+				tester.getMessage());
 	}
 
 	@Test
-	public void testAssertionFailedException() {
+	public void defaultExceptionMessage() {
 		AssertionFailedException tester = new AssertionFailedException();
-		assertEquals("AssertionFailedException.getMessage() returns a different default message than the constructor defaults", tester.getMessage(), defaultMessage);
+		assertEquals("AssertionFailedException.getMessage() returns a "
+				+ "different default message than the constructor defaults",
+				defaultMessage, tester.getMessage());
 	}
 }
