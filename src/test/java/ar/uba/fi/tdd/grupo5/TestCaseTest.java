d9c6a7a433ffb87c8e4ddb2d1f701b550aba7402
Diego R. Montoya <diegormontoya@hotmail.com>
2013-10-29 03:00:32 +0000

    Modifico los nombres de los test de TestCaseTest por unos más significativos, siguiendo la convención planteada

--- src/test/java/ar/uba/fi/tdd/grupo5/TestCaseTest.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/TestCaseTest.java
@@ -10,17 +10,17 @@ import ar.uba.fi.tdd.grupo5.test.AddTest;
 public class TestCaseTest {
 	
 	private TestCase tester;
-	private final String testName = "I'm a testAdd test";
+	private final String testName = "I'm a AddTest test";
 	private final String className = "AddTest";
 	
 	@Test
-	public void testTestCaseString() {
+	public void personalizedTestCaseName() {
 		tester = new AddTest(testName);
 		assertEquals("tester.getName() returns a different test name than the constructor name parameter", tester.getName(), testName);
 	}
 
 	@Test
-	public void testTestCase() {
+	public void defaultTestCaseName() {
 		tester = new AddTest();
 		assertEquals("tester.getName() returns a different test name than the constructor default", tester.getName(), className);
 	}
