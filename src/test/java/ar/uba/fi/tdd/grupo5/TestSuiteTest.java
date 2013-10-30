6a2e5414ef8faae40ac360350e31d66a6404deda
Diego R. Montoya <diegormontoya@hotmail.com>
2013-10-30 01:08:17 +0000

    Agrego el test que prueba el mensaje por default al correr un suite vacío

--- src/test/java/ar/uba/fi/tdd/grupo5/TestSuiteTest.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/TestSuiteTest.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.uba.fi.tdd.grupo5.framework.TestSuite;
+
 public class TestSuiteTest {
+
+	private TestSuite tester;
+	private final String emptySuite = "The TestSuite is empty. There are no tests to run.";
 
 	@Test
 	public void testTestSuite() {
@@ -17,8 +22,11 @@ public class TestSuiteTest {
 	}
 
 	@Test
-	public void testRun() {
-		fail("Not yet implemented");
+	public void runAnEmptyTestSuite() {
+		tester = new TestSuite("My Test Suite");
+		assertEquals(
+				"tester.run() returns a different default message than the expected",
+				emptySuite, tester.run());
 	}
 
 }
