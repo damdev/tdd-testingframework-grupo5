470d7c590c52e0b766dbb4abddcfa15a83614f2a
Ezequiel PD <ezeperez26@gmail.com>
2013-10-31 21:16:56 -0300

    Agrego la clase Test que se usará para el Composite de Test.

---
+++ src/main/java/ar/uba/fi/tdd/grupo5/framework/Test.java
@@ -0,0 +1,29 @@
+package ar.uba.fi.tdd.grupo5.framework;
+
+public abstract class Test {
+
+	protected String name;
+
+	/**
+	 * Sets up the fixture, for example, open a network connection. This method
+	 * is called before a test is executed.
+	 */
+	protected void setUp() throws Exception {
+	}
+
+	/**
+	 * Tears down the fixture, for example, close a network connection. This
+	 * method is called after a test is executed.
+	 */
+	protected void tearDown() throws Exception {
+	}
+
+	public abstract void run(TestResult result);
+
+	/**
+	 * Returns the {@code Test}'s name.
+	 */
+	public String getName() {
+		return name;
+	}
+}
