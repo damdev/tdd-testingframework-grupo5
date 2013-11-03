16d7a707689c96727488c67a9ab18d165f409128
Ezequiel PD <ezeperez26@gmail.com>
2013-11-02 22:09:29 -0300

    Corrijo el mensaje de test vacion en TestSuiteTest para que los test de JUnit4 corran todos.

--- src/test/java/ar/uba/fi/tdd/grupo5/TestSuiteTest.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/TestSuiteTest.java
@@ -9,11 +9,14 @@ import ar.uba.fi.tdd.grupo5.framework.TestSuite;
 public class TestSuiteTest {
 
 	private TestSuite tester;
-	private final String emptySuite = "The TestSuite is empty. There are no tests to run.";
+	private final String testSuiteName = "My Test Suite";
+	private final String emptySuite = testSuiteName
+			+ "\n­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­--------------------------\n"
+			+ "The TestSuite is empty. There are no tests to run.";
 
 	@Test
 	public void runAnEmptyTestSuite() {
-		tester = new TestSuite("My Test Suite");
+		tester = new TestSuite(testSuiteName);
 		assertEquals(
 				"tester.run() returns a different default message than the expected",
 				emptySuite, tester.run());
