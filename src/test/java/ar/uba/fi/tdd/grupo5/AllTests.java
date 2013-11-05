1c6ae5572c07ada6d907fec2c61cadd0db102c68
Ezequiel PD <ezeperez26@gmail.com>
2013-11-05 13:38:23 -0300

    Agrego RegexpTest a AllTests.

--- src/test/java/ar/uba/fi/tdd/grupo5/AllTests.java
+++ src/test/java/ar/uba/fi/tdd/grupo5/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ AssertExceptionTest.class, AssertTest.class, TimerTest.class,
 		ReportTest.class, TestResultTest.class, TestCaseTest.class,
-		TestSuiteTest.class, TwoLevelTestSuiteTest.class })
+		TestSuiteTest.class, RegexpTest.class, TwoLevelTestSuiteTest.class })
 
 public class AllTests {
 
