a71ba76126133ab18c2372546fc98485f77df1c3
Ezequiel PD <ezeperez26@gmail.com>
2013-11-10 19:16:37 -0300

    Agrego la posibilidad de que el Runner pueda correr una lista de Regular Expresions.

--- src/main/java/ar/uba/fi/tdd/grupo5/framework/Runner.java
+++ src/main/java/ar/uba/fi/tdd/grupo5/framework/Runner.java
@@ -1,36 +1,49 @@
 package ar.uba.fi.tdd.grupo5.framework;
 
+import java.util.Iterator;
+import java.util.List;
 import ar.uba.fi.tdd.grupo5.framework.tagmanager.*;
 
 public class Runner {
 
 	private TestSuite suite;
-	
+
 	public Runner(TestSuite suite) {
 		this.suite = suite;
 	}
-	
+
 	public Report runWithAnyTags(String tags) {
 		Criteria criteria = new AnyTag(tags);
 		return suite.run(criteria);
 	}
-	
+
 	public Report runWithAllTags(String tags) {
 		Criteria criteria = new AllTags(tags);
 		return suite.run(criteria);
 	}
-	
+
 	public Report runWithAllTagsAndRegexp(String tags, String regexp) {
 		MixedCriteria criteria = new MixedCriteria();
 		criteria.add(new AllTags(tags));
 		criteria.add(new Regexp(regexp));
 		return suite.run(criteria);
 	}
-	
+
 	public Report runWithAnyTagsAndRegexp(String tags, String regexp) {
 		MixedCriteria criteria = new MixedCriteria();
 		criteria.add(new AnyTag(tags));
 		criteria.add(new Regexp(regexp));
+		return suite.run(criteria);
+	}
+
+	public Report runWithAnyTagsAndRegexp(String tags, List<String> regexpList) {
+		MixedCriteria criteria = new MixedCriteria();
+		criteria.add(new AnyTag(tags));
+		Iterator<String> it = regexpList.iterator();
+		while (it.hasNext()) {
+			String regexp = it.next();
+			criteria.add(new Regexp(regexp));
+		}
 		return suite.run(criteria);
 	}
 }
